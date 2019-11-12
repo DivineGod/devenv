70a2201698af80013b1ea928ece0e274daae35df
Anders Rasmussen <divinegod@gmail.com>
2019-11-13 09:25:23 +1100

    try out starship.rs, not quite sure I like it yet

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -579,3 +579,5 @@ end
 # OPAM configuration
 # . /Users/voidedgin/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+# starship init fish | source
