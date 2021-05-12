b24b195c30ea0ce942fec6002a426ff9a4044c4a
Ando "Thor" Nando <divinegod@gmail.com>
2021-05-12 16:38:59 +1000

    use pillar

--- git/.config/git/helpers.sh
+++ git/.config/git/helpers.sh
@@ -8,15 +8,15 @@ DATE="%C(yellow)%ci%C(reset)" # %ci commit time
 REFS="%C(red)%d%C(reset)" # %d (decorate)
 SUBJECT="%s%C(reset)" # %s (subject)
 AUTHOR="%C(blue)<%an>%C(reset)" # %an (author name)
-FORMAT="$HASH{$REL_TIME{$REFS $SUBJECT $AUTHOR"
+FORMAT="{$HASH{$REL_TIME$REFS $SUBJECT $AUTHOR"
 
 function pretty_log() {
     # $* is splat params from invocation
-    git log --graph --pretty="tformat:$FORMAT" $* |
+    git log --graph --pretty="tformat:$FORMAT" --color $* |
     # -E is extended mode to allow (, ), +, ? without escaping
     sed -E 's/(,.+)? ago//' |
     # align by columns using { as delimiter
-    column -t -s '{' |
+    pillar |
     # page. quit if only one screen, don't clear, raw control chars, chop lines
     less -FXRS
 }
