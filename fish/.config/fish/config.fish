116a1a17fd7f01ba17dd39469dc144a76c26bc63
Ando "Thor" Nando <divinegod@gmail.com>
2021-12-09 15:07:35 +1100

    fish cargo rustup stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,8 @@
 set -g fish_key_bindings fish_vi_key_bindings
 
-source $HOME/.cargo/env
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+fish_add_path "$HOME/.cargo/bin"
+
+if status --is-interactive
+  eval (/opt/homebrew/bin/brew shellenv)
+end
