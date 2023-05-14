7d94c1a50de58da54f822aad0b0a2e72ad6569e6
Ando "Thor" Nando <divinegod@gmail.com>
2023-05-15 08:49:42 +1000

    fish: depthai

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,7 @@ fish_add_path "$HOME/.cargo/bin"
 fish_add_path "$HOME/.poetry/bin"
 fish_add_path "$HOME/go/bin"
 fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/Luxonis/depthai/entrypoint"
 
 if status --is-interactive
   set -x EDITOR "nvim"
